6bb5ebe2d6cd544934500645558a2c77f3c20cfe
yinka <holayinkajr@gmail.com>
2021-12-07 23:28:41 +0100

    setup server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const mailAPI = require("./routes/mail.js");
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     limit: "50mb",
+//     extended: false,
+//     parameterLimit: 50000
+//   })
+// );
+
+app.use("/api/v1", mailAPI);
+
+const port = process.env.PORT || 4444;
+
+app.listen(port, function () {
+  console.log(`listening to the port ${port} .....`);
+});
+    
