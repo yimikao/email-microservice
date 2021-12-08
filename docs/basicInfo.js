8253144e03d49934bfe0f7e959e3a7040eec483f
yinka <holayinkajr@gmail.com>
2021-12-08 13:55:36 +0100

    repo updated

---
+++ docs/basicInfo.js
@@ -0,0 +1,12 @@
+module.exports = {
+    openapi: "3.0.3", // present supported openapi version
+    info: {
+      title: "Email microservice", 
+      description: "Email microservice", 
+      version: "1.0.0", 
+      contact: {
+        name: "", 
+        email: "", 
+        url: "",       },
+    },
+  };
