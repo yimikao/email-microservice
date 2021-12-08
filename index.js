4904499dce3ed81e97ce8ab4d0a14d847f908e6c
yinka <holayinkajr@gmail.com>
2021-12-08 13:42:07 +0100

    add swagger

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ require('dotenv').config()
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const mailAPI = require("./routes/mail.js");
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express")
+
+const mailAPI = require("./app/routes/mail.js");
 app.use(cors());
 app.use(bodyParser.json());
-// app.use(
-//   bodyParser.urlencoded({
-//     limit: "50mb",
-//     extended: false,
-//     parameterLimit: 50000
-//   })
-// );
+
 
 app.use("/api/v1", mailAPI);
 
 const port = process.env.PORT || 4444;
+
+
+
 
 app.listen(port, function () {
   console.log(`listening to the port ${port} .....`);
